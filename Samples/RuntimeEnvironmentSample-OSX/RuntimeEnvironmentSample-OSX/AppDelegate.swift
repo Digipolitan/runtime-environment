93e451d633fb684892439b814f6316347ccb27a4
Benoit BRIATTE <briatte.benoit@digipolitan.com>
2018-12-11 01:18:36 +0100

    fix swift 4.2

--- Samples/RuntimeEnvironmentSample-OSX/RuntimeEnvironmentSample-OSX/AppDelegate.swift
+++ Samples/RuntimeEnvironmentSample-OSX/RuntimeEnvironmentSample-OSX/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var window: NSWindow!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
